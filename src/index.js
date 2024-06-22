a2187ac0a28fbe2880e3429e27fc386a6f94e560
Faizal Ahmad Siddiq <104670384+faizalahmads@users.noreply.github.com>
2024-06-22 21:01:14 +0700

    Penambahan CI-CD

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './App.jsx';
 import store from './states';
 
 import './assets/style.css';
