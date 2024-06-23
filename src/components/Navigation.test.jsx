c42d9c714daef99592da17be3748f70991d041b4
Faizal Ahmad Siddiq <104670384+faizalahmads@users.noreply.github.com>
2024-06-23 20:57:26 +0700

    make test error

--- src/components/Navigation.test.jsx
+++ src/components/Navigation.test.jsx
@@ -13,6 +13,6 @@ describe("Navigation", () => {
                 />
             </MemoryRouter>
         );
-        expect(component).toBeTruthy();
+        expect(component).toBeFalsy();
     });
 });
